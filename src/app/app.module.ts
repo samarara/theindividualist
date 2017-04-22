5403089652a0f16795167a3889631496ee580ca3
Sandes de Silva <sandes.samararatne@gmail.com>
2017-04-22 13:20:21 -0400

    added posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { TheIndividualistRoutingModule } from './app-routing.module'
 
 import { AppComponent } from './app.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { StyleMenComponent } from './style-men/style-men.component';
+import { StyleWomenComponent } from './style-women/style-women.component';
+import { EssaysComponent } from './essays/essays.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    StyleMenComponent,
+    StyleWomenComponent,
+    EssaysComponent,
+    AboutUsComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    TheIndividualistRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
